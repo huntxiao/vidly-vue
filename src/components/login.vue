fc55d974ecc4c7b690f7a6ccb45f226c2f05162e
huntxiao <huntxiao@gmail.com>
2019-07-10 00:27:51 +0800

    login layout and router configured

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,36 +1,44 @@
 <template>
   <div class="container">
+    <section class="jumbotron text-center">
+      <h1 class="jumbotron-heading display-3">Vidly</h1>
+      <p class="lead text-muted">This is a kind of thing you don't need to know</p>
+    </section>
     <form class="form-signin">
-      <div class="form-label-group">
+      <div class="form-group">
+        <label for="formGroupExampleInput">Email</label>
         <input
+          v-model="email"
           type="email"
-          id="inputEmail"
-          class="form-control"
-          placeholder="Email address"
+          :class="[{'is-invalid': emailIsValid},'form-control']"
+          id="InputEmail"
+          placeholder="Email"
           required
-          autofocus
         />
-        <label for="inputEmail">Email address</label>
+        <div class="invalid-feedback">{{emailValidMsg}}</div>
       </div>
-
-      <div class="form-label-group">
+      <div class="form-group">
+        <label for="formGroupExampleInput2">Password</label>
         <input
-          type="password"
-          id="inputPassword"
-          class="form-control"
+          v-model="password"
+          type="text"
+          :class="[{'is-invalid': passwordIsValid},'form-control']"
+          id="InputPassword"
           placeholder="Password"
           required
         />
-        <label for="inputPassword">Password</label>
+        <div class="invalid-feedback">{{passwordValidMsg}}</div>
       </div>
 
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me" /> Remember me
-        </label>
+      <div class="mt-2 mb-2 text-center">
+        <input type="checkbox" value="remember-me" /> Remember me
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
-      <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2018</p>
+      <hr />
+      <div class="mt-2 mb-2 text-center">
+        <a @click="toSignin" href>You don't have an Account?</a>
+      </div>
+      <button @click="login" class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
+      <p class="mt-5 mb-3 text-muted text-center">&copy;Copyrights for Hunt 2019-2020</p>
     </form>
   </div>
 </template>
@@ -39,89 +47,46 @@ export default {
   name: "Login",
   data() {
     return {
-      msg: ""
+      email: "",
+      password: "",
+      error: "",
+      emailIsValid: false,
+      passwordIsValid: false,
+      emailValidMsg: "",
+      passwordValidMsg: ""
     };
-  }
+  },
+  methods: {
+    login() {
+      if (this.email.length === 0) {
+        this.emailIsValid = true;
+        this.emailValidMsg = "Email can not be null";
+      }
+      if (this.password.length === 0) {
+        this.passwordIsValid = true;
+        this.passwordIsValid = "Password can not be null";
+      }
+    },
+    toSignin(event) {
+      event.preventDefault();
+      this.$router.push("/signin");
+    }
+  },
+  created() {},
+  mounted() {},
+  computed: {}
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-body {
-  display: -ms-flexbox;
-  display: -webkit-box;
-  display: flex;
-  -ms-flex-align: center;
-  -ms-flex-pack: center;
-  -webkit-box-align: center;
-  align-items: center;
-  -webkit-box-pack: center;
-  justify-content: center;
-  padding-top: 40px;
-  padding-bottom: 40px;
-  background-color: #f5f5f5;
+* {
+  font-family: -apple-system;
 }
-
-.form-signin {
-  width: 100%;
-  max-width: 420px;
-  padding: 15px;
-  margin: 0 auto;
-}
-
-.form-label-group {
-  position: relative;
-  margin-bottom: 1rem;
-}
-
-.form-label-group > input,
-.form-label-group > label {
-  padding: var(0.75rem) var(0.75rem);
-}
-
-.form-label-group > label {
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: block;
-  width: 100%;
-  margin-bottom: 0; /* Override default `<label>` margin */
-  line-height: 1.5;
-  color: #495057;
-  border: 1px solid transparent;
-  border-radius: 0.25rem;
-  transition: all 0.1s ease-in-out;
-}
-
-.form-label-group input::-webkit-input-placeholder {
-  color: transparent;
-}
-
-.form-label-group input:-ms-input-placeholder {
-  color: transparent;
-}
-
-.form-label-group input::-ms-input-placeholder {
-  color: transparent;
-}
-
-.form-label-group input::-moz-placeholder {
-  color: transparent;
-}
-
-.form-label-group input::placeholder {
-  color: transparent;
-}
-
-.form-label-group input:not(:placeholder-shown) {
-  padding-top: calc(var(0.75rem) + var(0.75rem) * (2 / 3));
-  padding-bottom: calc(var(0.75rem) / 3);
-}
-
-.form-label-group input:not(:placeholder-shown) ~ label {
-  padding-top: calc(var(0.75rem) / 3);
-  padding-bottom: calc(var(0.75rem) / 3);
-  font-size: 12px;
-  color: #777;
+.container {
+  margin-top: 7%;
+  width: 30%;
+  font-family: -apple-system;
+  font-weight: 320;
 }
 </style>
